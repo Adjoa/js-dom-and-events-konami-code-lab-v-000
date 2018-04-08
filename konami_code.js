07b6d7eedb6c9968191cb44e81e2f8f6a6160167
Adjoa Darien <adjoa.darien@gmail.com>
2018-04-08 17:17:10 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,40 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  const docBody = document.querySelector('body')
+  docBody.addEventListener("click", function() {
+    console.log("Hello, world!")
+  })
 }
+
+
+
+// // Key codes for A, B, and C keys.
+// const alphabet = [65, 66, 67];
+//
+// // Keep track of index outside of the event handler.
+// let index = 0;
+//
+// // This is the function that would be invoked by the event listener.
+// function onKeyDownHandler(e) {
+//   const key = parseInt(e.detail || e.key);
+//   console.log(key)
+//   console.log(e.location)
+//   if (key === alphabet[index]) {
+//     index++;
+//
+//     if (index === alphabet.length) {
+//       alert("Hurray!");
+//
+//       index = 0;
+//     }
+//   } else {
+//     index = 0;
+//   }
+// }
+//
+// function init() {
+//   // Write your JavaScript code inside the init() function
+//   const body = document.querySelector('body')
+//   body.addEventListener('keydown', onKeyDownHandler)
+// }
